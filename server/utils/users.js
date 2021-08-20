83bcc28dc18b07a3babceb7f1174eb80f9400455
gourav2001k <mailatgourav2001@gmail.com>
2021-08-20 13:25:00 +0530

    Configured Encryption Protocol

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,52 +1,71 @@
+const EthCrypto = require("eth-crypto");
+const dotenv = require("dotenv");
+dotenv.config();
 const users = [];
 
 // addUser, removeUser, getUser, getUsersInRoom
 
 const addUser = ({ id, username, room }) => {
-    // Clean the data
-    username = username.trim().toLowerCase();
-    room = room.trim().toLowerCase();
+  // Clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    // Validate the data
-    if (!username || !room) {
-        return { error: 'Username and Room are required!' };
-    }
+  // Validate the data
+  if (!username || !room) {
+    return { error: "Username and Room are required!" };
+  }
 
-    // Check the existing user
-    const existingUser = users.find((user) => {
-        return user.room === room && user.username === username;
-    });
+  // Check the existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+  //   check for already existing session keys
+  if (process.env["SK_" + room] === undefined) {
+    // If not present generate a pair
+    const sessionKeys = EthCrypto.createIdentity();
+    process.env["SK_" + room] = sessionKeys.privateKey;
+    process.env["PK_" + room] = sessionKeys.publicKey;
+  }
+  // Validating username
+  if (existingUser) {
+    return { error: "Username is in use !" };
+  }
 
-    // Validating username
-    if (existingUser) {
-        return { error: 'Username is in use !' };
-    }
-
-    // Store User
-    const user = { id, username, room };
-    users.push(user);
-    return { user };
-}
+  // Store User
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id);
-
-    if (index !== -1)
-        return users.splice(index, 1)[0];
-}
+  const index = users.findIndex((user) => user.id === id);
+  let removedUser;
+  if (index !== -1) {
+    removedUser = users.splice(index, 1)[0];
+    const usersInTheSameRoom = users.findIndex(
+      (user) => user.room === removedUser.room
+    );
+    if (usersInTheSameRoom === -1) {
+      if (process.env["SK_" + removedUser.room])
+        delete process.env["SK_" + removedUser.room];
+      if (process.env["PK_" + removedUser.room])
+        delete process.env["PK_" + removedUser.room];
+    }
+  }
+};
 
 const getUser = (id) => {
-    const user = users.find((user) => user.id === id);
-    return user;
+  const user = users.find((user) => user.id === id);
+  return user;
 };
 
 const getUsersInRoom = (room) => {
-    return users.filter((user) => user.room === room);
+  return users.filter((user) => user.room === room);
 };
 
 module.exports = {
-    addUser,
-    removeUser,
-    getUser,
-    getUsersInRoom
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
 };
--- src/pages/VHome.js
+++ src/pages/VHome.js
@@ -1,59 +1,72 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import EthCrypto from "eth-crypto";
 import { useSocket } from "../context/SocketProvider";
 import useChat from "../hooks/useChat";
 import "./Chat.css";
 
 function VHome() {
-	const [username, setUsername] = useState("");
-	const [room, setRoom] = useState("");
-	const { addMessage } = useChat();
-	const socket = useSocket();
-	const history = useHistory();
-	const joinRoom = (e) => {
-		e.preventDefault();
-		if (username !== "" && room !== "") {
-			socket.emit("join", { username, room }, (error) => {
-				if (error) {
-					alert("Username and Room are required!!", error);
-					history.push("/v2v");
-				}
-				socket.on("message", (msg) => {
-					addMessage(msg.text, "message", msg.username);
-				});
-			});
-			history.push(`/chat/${room}/${username}`);
-		}
-	};
+  const [sk, setSK] = useState("");
+  const [room, setRoom] = useState("");
+  const { addMessage } = useChat();
+  const socket = useSocket();
+  const history = useHistory();
 
-	return (
-		<div className="centered-form">
-			<div className="centered-form__box">
-				<h1>Join</h1>
-				<form onSubmit={joinRoom}>
-					<label>Display Name</label>
-					<input
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						name="username"
-						placeholder="display name"
-						required
-					/>
-					<label>Room</label>
-					<input
-						type="text"
-						name="room"
-						value={room}
-						onChange={(e) => setRoom(e.target.value)}
-						placeholder="Room"
-						required
-					/>
-					<button type="submit">Join</button>
-				</form>
-			</div>
-		</div>
-	);
+  const joinRoom = (e) => {
+    e.preventDefault();
+    if (sk !== "" && room !== "") {
+      const pk = EthCrypto.publicKeyByPrivateKey(sk);
+      sessionStorage.setItem("sK", sk);
+      sessionStorage.setItem("pK", pk);
+      socket.emit("join", { username: pk, room }, (error) => {
+        if (error) {
+          alert("Provide a valid Private Key", error);
+          history.push("/v2v");
+        }
+        // socket.on("message", (msg) => {
+        //   addMessage(msg.text, "message", msg.username);
+        // });
+        socket.on("session_key", async ({ keys }) => {
+          console.log("Hello");
+          console.log(keys);
+          var decrypted = await EthCrypto.decryptWithPrivateKey(sk, keys);
+          const { pK, sK } = JSON.parse(decrypted);
+          sessionStorage.setItem("sPK", pK);
+          sessionStorage.setItem("sSK", sK);
+          history.push(`/chat/${room}/${pk}`);
+        });
+      });
+    }
+  };
+
+  return (
+    <div className="centered-form">
+      <div className="centered-form__box">
+        <h1>Join</h1>
+        <form onSubmit={joinRoom}>
+          <label>Enter Private Key</label>
+          <input
+            type="text"
+            value={sk}
+            onChange={(e) => setSK(e.target.value)}
+            name="sk"
+            placeholder="Don't worry this key will remain in the browser only"
+            required
+          />
+          <label>Room</label>
+          <input
+            type="text"
+            name="room"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            placeholder="Room"
+            required
+          />
+          <button type="submit">Join</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default VHome;
