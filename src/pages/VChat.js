1838603a9b81e3c48168500fa988418f747805e4
Harish Sharma <hks212001@gmail.com>
2021-09-02 22:57:47 +0530

    Added Message Dropdown

--- src/pages/VChat.js
+++ src/pages/VChat.js
@@ -1,153 +1,189 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import { useParams } from "react-router";
 import { useSocket } from "../context/SocketProvider";
+import { makeStyles } from "@material-ui/core/styles";
 import EthCrypto from "eth-crypto";
+import Button from "@material-ui/core/Button";
 import useChat from "../hooks/useChat";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import { MainContext } from "../context";
 // import useLocation from "../hooks/useLocation";
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		margin: "auto",
+	},
+	paper: {
+		width: 300,
+		height: 230,
+		overflow: "auto",
+	},
+	button: {
+		// margin: theme.spacing(0, 0),
+		padding: 0,
+	},
+	formControl: {
+		margin: theme.spacing(2),
+		minWidth: "80%",
+	},
+	selectEmpty: {
+		marginTop: theme.spacing(2),
+		padding: 10,
+	},
+}));
+
 function VChat() {
-  const { room } = useParams();
-  const socket = useSocket();
-  const chatRef = useRef(null);
-  const { messages, addMessage } = useChat();
-  // const { getCurrentLocation } = useLocation();
-  const [message, setMessage] = useState("");
-  const [users, setUsers] = useState([]);
+	const { room } = useParams();
+	const socket = useSocket();
+	const classes = useStyles();
+	const chatRef = useRef(null);
+	const { messages, addMessage } = useChat();
+	const { messages: rsuMessages } = useContext(MainContext);
+	// const { getCurrentLocation } = useLocation();
+	const [message, setMessage] = useState("");
+	const [users, setUsers] = useState([]);
+	//   const custom = async () => {
+	//     const address = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
+	//     const sk =
+	//       "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+	//     const pk = EthCrypto.publicKeyByPrivateKey(sk);
+	//     console.log("Address", address);
+	//     console.log("PublicKey", pk);
+	//     console.log("SecretKey", sk);
+	//     const message = "Not that old  Hello World shit like every body else";
+	//     const messageHash = EthCrypto.hash.keccak256(message);
+	//     const signature = EthCrypto.sign(sk, messageHash);
+	//     console.log("Signature", signature);
+	//     const signer = EthCrypto.recoverPublicKey(signature, messageHash);
+	//     console.log("Signer", signer);
+	//     const encrypted = await EthCrypto.encryptWithPublicKey(pk, message);
+	//     const decrypted = await EthCrypto.decryptWithPrivateKey(sk, encrypted);
+	//     console.log("Encryped", encrypted);
+	//     console.log("Decrypted", decrypted);
+	//   };
 
-  //   const custom = async () => {
-  //     const address = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
-  //     const sk =
-  //       "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
-  //     const pk = EthCrypto.publicKeyByPrivateKey(sk);
-  //     console.log("Address", address);
-  //     console.log("PublicKey", pk);
-  //     console.log("SecretKey", sk);
-  //     const message = "Not that old  Hello World shit like every body else";
-  //     const messageHash = EthCrypto.hash.keccak256(message);
-  //     const signature = EthCrypto.sign(sk, messageHash);
-  //     console.log("Signature", signature);
-  //     const signer = EthCrypto.recoverPublicKey(signature, messageHash);
-  //     console.log("Signer", signer);
-  //     const encrypted = await EthCrypto.encryptWithPublicKey(pk, message);
-  //     const decrypted = await EthCrypto.decryptWithPrivateKey(sk, encrypted);
-  //     console.log("Encryped", encrypted);
-  //     console.log("Decrypted", decrypted);
-  //   };
+	useEffect(() => {
+		if (chatRef) {
+			chatRef.current.addEventListener("DOMNodeInserted", (event) => {
+				const { currentTarget: target } = event;
+				target.scroll({ top: target.scrollHeight, behavior: "smooth" });
+			});
+		}
+	}, []);
 
-  useEffect(() => {
-    if (chatRef) {
-      chatRef.current.addEventListener("DOMNodeInserted", (event) => {
-        const { currentTarget: target } = event;
-        target.scroll({ top: target.scrollHeight, behavior: "smooth" });
-      });
-    }
-  }, []);
+	useEffect(() => {
+		socket.on("message", async (msg) => {
+			if (!msg) return;
+			const sSK = sessionStorage.getItem("sSK");
+			const { cipher, sign } = JSON.parse(msg.text);
+			const data = await EthCrypto.decryptWithPrivateKey(sSK, cipher);
+			const messageHash = EthCrypto.hash.keccak256(data);
+			const signer = EthCrypto.recoverPublicKey(sign, messageHash);
+			if (signer !== msg.username) return;
+			addMessage(data, "message", msg.username, msg.createdAt);
+		});
+		return () => socket.off("message");
+	}, [socket, addMessage]);
 
-  useEffect(() => {
-    socket.on("message", async (msg) => {
-      if (!msg) return;
-      const sSK = sessionStorage.getItem("sSK");
-      const { cipher, sign } = JSON.parse(msg.text);
-      const data = await EthCrypto.decryptWithPrivateKey(sSK, cipher);
-      const messageHash = EthCrypto.hash.keccak256(data);
-      const signer = EthCrypto.recoverPublicKey(sign, messageHash);
-      if (signer !== msg.username) return;
-      addMessage(data, "message", msg.username, msg.createdAt);
-    });
-    return () => socket.off("message");
-  }, [socket, addMessage]);
+	useEffect(() => {
+		socket.on("admin", (msg) => {
+			console.log("VCHAt");
+			if (!msg) return;
+			addMessage(msg.text, "message", msg.username);
+		});
+		return () => socket.off("admin");
+	}, [socket, addMessage]);
 
-  useEffect(() => {
-    socket.on("admin", (msg) => {
-      console.log("VCHAt");
-      if (!msg) return;
-      addMessage(msg.text, "message", msg.username);
-    });
-    return () => socket.off("admin");
-  }, [socket, addMessage]);
+	useEffect(() => {
+		socket.on("locationMessage", (msg) => {
+			if (msg) {
+				addMessage(msg.url, "location", msg.username, msg.createdAt);
+			}
+		});
+		return () => socket.off("locationMessage");
+	}, [socket, addMessage]);
 
-  useEffect(() => {
-    socket.on("locationMessage", (msg) => {
-      if (msg) {
-        addMessage(msg.url, "location", msg.username, msg.createdAt);
-      }
-    });
-    return () => socket.off("locationMessage");
-  }, [socket, addMessage]);
+	useEffect(() => {
+		socket.on("roomData", ({ users }) => setUsers(users));
+		return () => socket.off("roomData");
+	}, [socket]);
 
-  useEffect(() => {
-    socket.on("roomData", ({ users }) => setUsers(users));
-    return () => socket.off("roomData");
-  }, [socket]);
+	const sendMessage = async (e) => {
+		e.preventDefault();
+		const messageHash = EthCrypto.hash.keccak256(message);
+		const sk = sessionStorage.getItem("sK");
+		const sPK = sessionStorage.getItem("sPK");
+		const signature = EthCrypto.sign(sk, messageHash);
+		const encrypted = await EthCrypto.encryptWithPublicKey(sPK, message);
+		const packet = { cipher: encrypted, sign: signature };
+		socket.emit("sendMessage", JSON.stringify(packet), () => null);
+		setMessage("");
+	};
 
-  const sendMessage = async (e) => {
-    e.preventDefault();
-    const messageHash = EthCrypto.hash.keccak256(message);
-    const sk = sessionStorage.getItem("sK");
-    const sPK = sessionStorage.getItem("sPK");
-    const signature = EthCrypto.sign(sk, messageHash);
-    console.log(sPK);
-    const encrypted = await EthCrypto.encryptWithPublicKey(sPK, message);
-    const packet = { cipher: encrypted, sign: signature };
-    socket.emit("sendMessage", JSON.stringify(packet), () => null);
-    setMessage("");
-  };
+	return (
+		<>
+			<div className="chat">
+				<div className="chat__sidebar" id="sidebar">
+					<h2 className="room-title">{room}</h2>
+					<h3 className="list-title">Users</h3>
+					<ul className="users">
+						{users &&
+							users.length > 0 &&
+							users.map(({ username, id, room: r }, idx) => {
+								if (room === r)
+									return (
+										<li
+											style={{ color: "aquamarine" }}
+											key={idx}
+										>
+											{username.substr(0, 15) + "..."}
+										</li>
+									);
+								return <li key={idx}></li>;
+							})}
+					</ul>
+				</div>
+				<div className="chat__main">
+					<h1 style={{ margin: 20 }}>V2V Chat</h1>
+					<div id="messages" className="chat__messages" ref={chatRef}>
+						{messages &&
+							messages.length > 0 &&
+							messages.map(
+								({ text, type, createdAt, user }, idx) => (
+									<div className="message" key={idx}>
+										<p>
+											<span className="message__name">
+												{user.substr(0, 15) + "..."}
+											</span>
+											<span className="message__meta">
+												{" "}
+												{createdAt}
+											</span>
+										</p>
+										{type === "message" ? (
+											<p>{text}</p>
+										) : (
+											<p>
+												<a
+													href={text}
+													target="_blank"
+													rel="noreferrer"
+												>
+													My Current Location
+												</a>
+											</p>
+										)}
+									</div>
+								)
+							)}
+					</div>
 
-  // const sendLocation = () => {
-  //   let pos = getCurrentLocation();
-  //   if (pos && pos.latitude && pos.longitude)
-  //     socket.emit("sendLocation", pos, () => {});
-  // };
-
-  return (
-    <>
-      <div className="chat">
-        <div className="chat__sidebar" id="sidebar">
-          <h2 className="room-title">{room}</h2>
-          <h3 className="list-title">Users</h3>
-          <ul className="users">
-            {users &&
-              users.length > 0 &&
-              users.map(({ username, id, room: r }, idx) => {
-                if (room === r)
-                  return (
-                    <li style={{ color: "aquamarine" }} key={idx}>
-                      {username.substr(0, 15) + "..."}
-                    </li>
-                  );
-                return <li key={idx}></li>;
-              })}
-          </ul>
-        </div>
-        <div className="chat__main">
-          <h1 style={{ margin: 20 }}>V2V Chat</h1>
-          <div id="messages" className="chat__messages" ref={chatRef}>
-            {messages &&
-              messages.length > 0 &&
-              messages.map(({ text, type, createdAt, user }, idx) => (
-                <div className="message" key={idx}>
-                  <p>
-                    <span className="message__name">
-                      {user.substr(0, 15) + "..."}
-                    </span>
-                    <span className="message__meta"> {createdAt}</span>
-                  </p>
-                  {type === "message" ? (
-                    <p>{text}</p>
-                  ) : (
-                    <p>
-                      <a href={text} target="_blank" rel="noreferrer">
-                        My Current Location
-                      </a>
-                    </p>
-                  )}
-                </div>
-              ))}
-          </div>
-
-          <div className="compose">
-            <form id="message-form" onSubmit={sendMessage}>
-              <input
+					<div className="compose">
+						<form id="message-form" onSubmit={sendMessage}>
+							{/* <input
                 name="message"
                 value={message}
                 placeholder="Type here"
@@ -155,13 +191,32 @@ function VChat() {
                 required
                 autoComplete="off"
               />
-              <button type="submit">Send</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+              <button type="submit">Send</button> */}
+							<FormControl className={classes.formControl}>
+								<InputLabel id="demo-simple-select-label">
+									Select Message
+								</InputLabel>
+								<Select
+									labelId="demo-simple-select-label"
+									id="demo-simple-select"
+									value={message}
+									required
+									onChange={(e) => setMessage(e.target.value)}
+								>
+									{rsuMessages.map((message, idx) => (
+										<MenuItem key={idx} value={message}>
+											{message}
+										</MenuItem>
+									))}
+								</Select>
+							</FormControl>
+							<Button type="submit">Send Message</Button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default VChat;
