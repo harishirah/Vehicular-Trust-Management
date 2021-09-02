37956a761a41172e25f49e15f4afd7c70c55d373
gourav2001k <mailatgourav2001@gmail.com>
2021-09-02 21:48:37 +0530

    Location based Room Allotment

--- server/index.js
+++ server/index.js
@@ -6,22 +6,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const {
-	generateMessages,
-	generateLocationMessage,
-	sendKeys,
+  generateMessages,
+  generateLocationMessage,
+  sendKeys,
 } = require("./utils/messages");
 const {
-	addUser,
-	removeUser,
-	getUser,
-	getUsersInRoom,
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
 } = require("./utils/users");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
-	cors: {
-		origin: "*",
-	},
+  cors: {
+    origin: "*",
+  },
 });
 
 // app.use(cors());
@@ -33,85 +33,82 @@ const trustRoutes = require("./routes/trust");
 app.use("", trustRoutes);
 
 io.on("connection", (socket) => {
-	console.log("New Socket User Connected");
+  console.log("New Socket User Connected");
 
-	socket.on("join", ({ username, room }, callback) => {
-		const { error, user } = addUser({ id: socket.id, username, room });
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
 
-		if (error) {
-			return callback(error);
-		}
-		socket.join(user.room);
-		socket.emit("admin", generateMessages("Admin", "Welcome!"));
-		// Send Public/Private Key pair to user by encrypting it with user's public key
-		var msg = {
-			pK: process.env["PK_" + room],
-			sK: process.env["SK_" + room],
-		};
-		sendKeys(JSON.stringify(msg), username, username).then((keys) => {
-			socket.emit("session_key", keys);
-		});
-		socket.broadcast
-			.to(user.room)
-			.emit(
-				"admin",
-				generateMessages(
-					"Admin",
-					user.username + " has joined the ChatRoom"
-				)
-			);
-		io.to(user.room).emit("roomData", {
-			room: user.room,
-			users: getUsersInRoom(user.room),
-		});
-	});
+    if (error) {
+      return callback(error);
+    }
+    socket.join(user.room);
+    socket.emit("admin", generateMessages("Admin", "Welcome!"));
+    // Send Public/Private Key pair to user by encrypting it with user's public key
+    var msg = {
+      pK: process.env["PK_" + room],
+      sK: process.env["SK_" + room],
+    };
+    sendKeys(JSON.stringify(msg), username, username).then((keys) => {
+      socket.emit("session_key", keys);
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "admin",
+        generateMessages(
+          "Admin",
+          user.username.substr(0, 30) + "... has joined the ChatRoom"
+        )
+      );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+  });
 
-	socket.on("sendMessage", (message, callback) => {
-		const user = getUser(socket.id);
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
 
-		io.to(user.room).emit(
-			"message",
-			generateMessages(user.username, message)
-		);
+    io.to(user.room).emit("message", generateMessages(user.username, message));
 
-		callback("");
-	});
+    callback("");
+  });
 
-	socket.on("sendLocation", ({ latitude, longitude }, callback) => {
-		const user = getUser(socket.id);
+  socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+    const user = getUser(socket.id);
 
-		io.to(user.room).emit(
-			"locationMessage",
-			generateLocationMessage(
-				user.username,
-				"https://google.com/maps?q=" + latitude + "," + longitude
-			)
-		);
+    io.to(user.room).emit(
+      "locationMessage",
+      generateLocationMessage(
+        user.username,
+        "https://google.com/maps?q=" + latitude + "," + longitude
+      )
+    );
 
-		callback("");
-	});
+    callback("");
+  });
 
-	socket.on("disconnect", () => {
-		const user = removeUser(socket.id);
-		if (user) {
-			socket.broadcast
-				.to(user.room)
-				.emit(
-					"admin",
-					generateMessages(
-						"Admin",
-						user.username + " has left the ChatRoom"
-					)
-				);
-			io.to(user.room).emit("roomData", {
-				room: user.room,
-				users: getUsersInRoom(user.room),
-			});
-		}
-	});
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          "admin",
+          generateMessages(
+            "Admin",
+            user.username.substr(0, 30) + "... has left the ChatRoom"
+          )
+        );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
 });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-	console.log(`Server up and running on port ${port}`);
+  console.log(`Server up and running on port ${port}`);
 });
