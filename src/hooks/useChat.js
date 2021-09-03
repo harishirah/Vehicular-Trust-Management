bea3c81d2a1c9e6aa35c8239867d9f34c9c3c8ac
gourav2001k <mailatgourav2001@gmail.com>
2021-09-03 21:46:13 +0530

    Event Based Message Grouping

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,34 +1,33 @@
 import { useState } from "react";
 
 export default function useChat() {
-	const [messages, setMessages] = useState([]);
-	const addMessage = (msgStruct, user, createdAt) => {
-		const message = {
-			struct: msgStruct,
-			user,
-			createdAt,
-		};
-		if (messages.lastItem === message) return messages.length - 1;
-		const length = messages.length;
-		setMessages((prevMessages) => [...prevMessages, message]);
-		return length;
-	};
+    const [messages, setMessages] = useState([]);
+    const addMessage = (msgStruct, user, createdAt) => {
+        const message = {
+            struct: msgStruct,
+            user,
+            createdAt,
+        };
+        if (messages.lastItem === message) return messages.length - 1;
+        const length = messages.length;
+        setMessages((prevMessages) => [...prevMessages, message]);
+        return length;
+    };
 
-	const updateStatus = (idx) => {
-		setMessages((prevMessages) => {
-			prevMessages[idx].struct.status = 0;
-			return prevMessages;
-		});
-	};
+    const updateStatus = (idx) => {
+        setMessages((prevMessages) => {
+            prevMessages[idx].struct.status = 0;
+            return prevMessages;
+        });
+    };
 
-	const updateResponse = (idx, resp) => {
-		console.log(messages);
-		console.log(idx, resp);
-		setMessages((prevMessages) => {
-			prevMessages[idx].struct.response = resp;
-			return prevMessages;
-		});
-	};
+    const updateResponse = (idx, resp) => {
+        setMessages((prevMessages) => {
+            prevMessages[idx].struct.response = resp;
+            prevMessages[idx].struct.status = 0;
+            return prevMessages;
+        });
+    };
 
-	return { messages, addMessage, updateStatus, updateResponse };
+    return { messages, addMessage, updateStatus, updateResponse };
 }
